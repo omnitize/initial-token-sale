48ab96625e5881bb1369fccfa4462093ba9b35b2
Gadi Guy <gadiguy@Gadis-MacBook-Pro.local>
2017-09-05 11:43:55 +0300

    added global state object

---
+++ src/frontend/state.ts
@@ -0,0 +1,39 @@
+
+export enum UserFlow {
+	CONTRIBUTE,
+	CHECK_BALANCE
+};
+
+export enum TxStatus {
+	PENDING,
+	VERIFIED,
+	FINAL
+};
+
+export class Transaction {
+	datetime: Date;
+	value: number;
+	currency: string;
+	price: number;
+	tokens: number;
+	status: TxStatus;
+}
+
+export class State {
+
+	useCase: UserFlow;
+	stepNumber: number;
+
+	captchaToken: string;
+
+	alreadyHaveWallet: boolean;
+
+	targetAddress: string;
+
+	targetBIP39: string;
+	targetWallet: string;
+
+	fundAddresses: Map<string, string>;
+
+	transactions: Array<Transaction>;
+};
