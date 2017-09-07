6f0c7c95aa69ad3a6e1639f4cb10cd9373f7ccb6
Gadi Guy <gadiguy@gmail.com>
2017-09-07 08:57:50 +0300

    Gadi addresses 1 (#3)
    * added code to get addresses

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -29,34 +29,52 @@ function query(sql: string, params: any): Promise<any> {
 
 interface Params {
 	captcha?: string;
+	sessionToken?: string;
+	targetAddress?: string;
 }
 
 export function setupApi(app: express.Application) {
 	app.use('/api/createSession', (req: express.Request, res: express.Response) => {
 		createSession((req.query as Params).captcha, req.connection.remoteAddress)
-		.then(obj => {
-			res.json(obj);
-		});
+		.then(obj => res.json(obj))
+		.catch(er => res.status(500).send(er));
+	});
+	app.use('/api/sendTargetAddress', (req: express.Request, res: express.Response) => {
+		sendTargetAddress((req.query as Params).sessionToken, (req.query as Params).targetAddress)
+		.then(obj => res.json(obj))
+		.catch(er => res.status(500).send(er));
 	});
 }
 
 export function createSession(captcha: string, remoteIp: string): Promise<{ sessionToken: string }> {
-
 	var data = new FormData()
 	data.append('secret', config.recaptchaSiteSecret);
 	data.append('response', captcha);
 	data.append('remoteIp', remoteIp);
-
 	return fetch(config.recaptchaUrl, {
 		method: 'POST',
 		body: data
 	})
-	.then((response: any) => response.json())
-	.then((result: any) => {
-		console.log(result);
-		if(!result.success) throw result;
+	.then((response: any) => {
+		if(!response.json().success) throw new Error('Captcha failed');
 		const sessionToken = base64url(crypto.randomBytes(config.sessionTokenSize));
 		return query('insert into sessions (session_id) values (?)', [ sessionToken ])
 		.then(() => { return { sessionToken }; });
 	});
 }
+
+export function sendTargetAddress(sessionToken: string, targetAddress: string): Promise<{ fundAddresses: { [key: string]: string } }> {
+	return Promise.resolve()
+	.then(() => query('update sessions set target_address=? where session_id=? and ( target_address=? or target_address is NULL )', [ targetAddress, sessionToken, targetAddress ]))
+	.then(results => { if (results.affectedRows != 1) throw new Error('Invariant violation'); })
+	.then(() => query('insert into addresses_inc (session_id) values (?)', [ sessionToken ]))
+	.then(results => query('update addresses set session_id=? where id=?', [ sessionToken, results.insertId]))
+	.then(() => query('select * from addresses where session_id=?', [ sessionToken ]))
+	.then(results => { 
+		if(results.length != 1) throw new Error('Invariant violation');
+		return { fundAddresses: { 'bitcoin': results[0].bitcoin, 'ether': results[0].ether } }; 
+	});
+}
+
+
+
