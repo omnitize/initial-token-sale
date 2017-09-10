d71ba7c156277f864c5fb07696c436a5208f3968
Gadi Guy <gadiguy@gmail.com>
2017-09-10 19:41:57 +0300

    added please wait text to create wallet

--- src/frontend/components/contribute/step-2/sub-steps/create-wallet.tsx
+++ src/frontend/components/contribute/step-2/sub-steps/create-wallet.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { createWalletContent as content } from '../../../../data/text-data';
 import { ButtonText, ButtonMain, InputCheckbox } from '../../../../common';
-import { incrementStep, setSubStep, setSubStepMounted, checkWrittenMnemonicPhrase } from '../../../../state';
+import { incrementStep, setSubStep, setSubStepMounted, checkWrittenMnemonicPhrase, createWallet } from '../../../../state';
 import { EWhereToSendFundsSubSteps, State } from '../../../../models';
 
 interface ICreateWalletProps {
@@ -16,20 +16,35 @@ export class CreateWallet extends React.Component<ICreateWalletProps, any> {
 
     componentDidMount() {
         setSubStepMounted(EWhereToSendFundsSubSteps.CREATE_WALLET)
+        if(!this.props.state.targetWallet) {
+            setTimeout(createWallet, 100);
+        }
     }
 
     render(): JSX.Element {
+        debugger;
+        if(!this.props.state.targetWallet) {
+            return (
+                <div
+                    className="its-create-wallet --its-transition-opacity"
+                    style={this.fadeTransitionStyle()}>
+                    <p>{content.pleaseWait}</p>
+                </div>
+            );
+        }
         return (
             <div
                 className="its-create-wallet --its-transition-opacity"
                 style={this.fadeTransitionStyle()}>
                 <p>{content.paragraph}</p>
+                {this.props.state.targetMnemonicPhrase}
                 <p>{content.paragraph2}</p>
                 <ButtonText onClick={this.handleDownloadClick}>
                     {content.buttonText}
                 </ButtonText>
                 <p>{content.paragraph3}</p>
                 <h4>{content.heading}</h4>
+                {this.props.state.targetAddress}
                 <InputCheckbox
                     name={content.inputCheckbox.name}
                     paragraph={content.inputCheckbox.paragraph}
