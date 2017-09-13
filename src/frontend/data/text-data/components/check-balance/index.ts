8b142ceead8cd3c1f84d7b1b02629996ad4a7591
Andrew Bryson <modernenglishcoaching@gmail.com>
2017-09-13 07:42:14 +0200

    restructured files (#17)

--- src/frontend/data/text-data/components/check-balance/index.ts
+++ src/frontend/data/text-data/components/check-balance/index.ts
@@ -1,4 +1,4 @@
-export { checkBalanceStartContent } from "./step-1/check-balance-start";
-export { checkWalletContent } from "./step-2/check-wallet";
-export { reclaimWalletContent } from "./step-2/sub-steps/reclaim-wallet";
-export { viewWalletHistoryContent } from "./step-3/view-wallet-history";
+export { checkBalanceStartContent } from "./check-balance-start";
+export { checkWalletContent } from "./check-balance/check-wallet";
+export { reclaimWalletContent } from "./check-balance/sub-steps/reclaim-wallet";
+export { viewWalletHistoryContent } from "./view-wallet-history";
